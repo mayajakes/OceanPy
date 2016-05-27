c61f3cc98a909eb930c0a748daa0b7c12b245e8b
janjaapmeijer <janjaapmeijer@gmail.com>
2016-05-27 22:36:57 +0200

    Functions for handling GIS shapefiles in python.

---
+++ shapefiles.py
@@ -0,0 +1,37 @@
+from OceanPy.projections import *
+import pyproj
+
+import shapefile
+from shapely.geometry import Polygon, Point
+
+
+def coords_from_line(shpfile_line, projection=None):
+    coords = []
+    sf_line = shapefile.Reader(shpfile_line)
+    for shape in sf_line.shapes():
+        for x, y in shape.points:
+            if projection is not None:
+                coords.append(pyproj.transform(projection, WGS84, x, y))
+            else:
+                coords.append(x, y)
+    return coords
+
+
+def coords_in_polygon(shpfile_points, shpfile_polygons, projection=None):
+
+    coords = []
+
+    sf_points = shpfile_points
+    # sf_points = shapefile.Reader(shpfile_points)
+    sf_polygons = shapefile.Reader(shpfile_polygons)
+
+    for shape in sf_points.shapes():
+        if projection is not None:
+            x, y = shape.points[0]
+            point = Point(pyproj.transform(projection, WGS84, x, y))
+        else:
+            point = Point(shape.points[0])
+        for poly in sf_polygons.shapes():
+            if Polygon(poly.points).contains(point):
+                coords.append(point.coords[0])
+    return coords
