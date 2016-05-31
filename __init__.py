8e975bce8d87c1396624f115767a87a49c1f8afd
janjaapmeijer <jaap.meijer@sei-international.org>
2016-05-31 08:23:53 +0200

    Update references in __init__.

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#__all__ = ['folder1', 'folder2'] # for folders within OceanPy
+
+from OceanPy.readwrite import *
+from OceanPy.projections import *
+from OceanPy.shapefiles import *
+from OceanPy.find import *
