4ac606d2a3205546f2110500b8e1b287417a36dc
janjaapmeijer <janjaapmeijer@gmail.com>
2016-06-02 16:21:59 +0200

    Update references in __init__.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,14 @@
 #__all__ = ['folder1', 'folder2'] # for folders within OceanPy
 
-from OceanPy.readwrite import *
-from OceanPy.projections import *
-from OceanPy.shapefiles import *
 from OceanPy.colormaps import *
+from OceanPy.extreme_value_analysis import *
 from OceanPy.find import *
+from OceanPy.interpolation import *
+from OceanPy.maps import *
+from OceanPy.meteorology import *
+from OceanPy.nc_animation import *
+from OceanPy.plot import *
+from OceanPy.projections import *
+from OceanPy.readwrite import *
+from OceanPy.shapefiles import *
+from OceanPy.ssh import *
