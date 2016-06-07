5d7ff5b0338059fd842ceb77bf72b8a2843a7074
janjaapmeijer <jaap.meijer@sei-international.org>
2016-06-07 18:33:32 +0200

    Add function to change coordinate system for polygon shapefile.

--- shapefiles.py
+++ shapefiles.py
@@ -1,4 +1,4 @@
-from OceanPy.projections import *
+import OceanPy as op
 import pyproj
 
 import shapefile
@@ -11,7 +11,7 @@ def coords_from_line(shpfile_line, projection=None):
     for shape in sf_line.shapes():
         for x, y in shape.points:
             if projection is not None:
-                coords.append(pyproj.transform(projection, WGS84, x, y))
+                coords.append(pyproj.transform(projection, op.WGS84, x, y))
             else:
                 coords.append(x, y)
     return coords
@@ -31,10 +31,27 @@ def coords_in_polygon(shpfile_points, shpfile_polygons, projection=None):
     for shape in sf_points.shapes():
         if projection is not None:
             x, y = shape.points[0]
-            point = Point(pyproj.transform(projection, WGS84, x, y))
+            point = Point(pyproj.transform(projection, op.WGS84, x, y))
         else:
             point = Point(shape.points[0])
         for poly in sf_polygons.shapes():
             if Polygon(poly.points).contains(point):
                 coords.append(point.coords[0])
     return coords
+
+def convert_polygons(shpfile_polygons, projin, projout):
+
+    try:
+        sf_polygons = shapefile.Reader(shpfile_polygons)
+    except shapefile.ShapefileException:
+        sf_polygons = shpfile_polygons
+
+    polygons = shapefile.Writer(shapefile.POLYGON)
+    for shape in sf_polygons.shapes():
+        points = shape.points
+        poly = []
+        for point in points:
+            poly.append(list(pyproj.transform(projin, projout, point[0], point[1])))
+        polygons.poly(parts=[poly])
+
+    return polygons
