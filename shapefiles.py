fdb94a16c333e49e372479725454eda1a4cc1f84
janjaapmeijer <jaap.meijer@sei-international.org>
2016-06-08 19:57:56 +0200

    Implementing class Shapefile.

--- shapefiles.py
+++ shapefiles.py
@@ -42,18 +42,6 @@ class Shapefile(object):
                     poly.append(list(pyproj.transform(self.projin, self.projout, point[0], point[1])))
                 self.writer.poly(parts=[poly])
         return self.writer
-        # def transform(self, projin=None, projout=None):
-
-# def coords_from_line(shpfile_line, projection=None):
-#     coords = []
-#     sf_line = shapefile.Reader(shpfile_line)
-#     for shape in sf_line.shapes():
-#         for x, y in shape.points:
-#             if projection is not None:
-#                 coords.append(pyproj.transform(projection, op.WGS84, x, y))
-#             else:
-#                 coords.append((x, y))
-#     return coords
 
 
 def coords_in_polygon(shpfile_points, shpfile_polygons, projection=None):
@@ -82,21 +70,3 @@ def coords_in_polygon(shpfile_points, shpfile_polygons, projection=None):
                 coords.append(point.coords[0])
     return coords
 
-def transform_polygons(shpfile_polygons, projin, projout):
-
-    try:
-        sf_polygons = shapefile.Reader(shpfile_polygons)
-    except shapefile.ShapefileException:
-        sf_polygons = shpfile_polygons
-
-    polygons = shapefile.Writer(shapefile.POLYGON)
-    for shape in sf_polygons.shapes():
-        points = shape.points
-        poly = []
-        for point in points:
-            poly.append(list(pyproj.transform(projin, projout, point[0], point[1])))
-        polygons.poly(parts=[poly])
-
-    return polygons
-
-# def transform_points(shpfile_points, projin, projout):
