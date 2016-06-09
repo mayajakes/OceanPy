45d3da69f863d6cca80b984970d13271e5089d25
janjaapmeijer <jaap.meijer@sei-international.org>
2016-06-09 14:26:13 +0200

    Implementing class Shapefile.

--- shapefiles.py
+++ shapefiles.py
@@ -2,7 +2,8 @@ import OceanPy as op
 import pyproj
 
 import shapefile
-from shapely.geometry import Polygon, Point
+import shapely.geometry as slgeo
+import matplotlib.patches as mplpatch
 
 class Shapefile(object):
 
@@ -30,6 +31,21 @@ class Shapefile(object):
                 else:
                     self.coordinates.append((x, y))
 
+    def get_patch(self):
+        if self.reader.shapeType == 5:
+            self.read_shpfile()
+            self.patches = []
+            if self.projin is not None and self.projout is not None:
+                self.transform_shapes()
+                for shape in self.writer.shapes():
+                    mplpatch.Polygon(shape.points)
+            else:
+                for shape in self.reader.shapes():
+                    mplpatch.Polygon(shape.points)
+        else:
+            pass
+
+
     def transform_shapes(self):
         if self.reader.shapeType == 1 or self.reader.shapeType == 3:
             pass
@@ -44,29 +60,45 @@ class Shapefile(object):
         return self.writer
 
 
-def coords_in_polygon(shpfile_points, shpfile_polygons, projection=None):
+def shapes_in_polygons(shpfile_shapes, shpfile_polygons):
 
     coords = []
-
-    try:
-        sf_points = shapefile.Reader(shpfile_points)
-        sf_polygons = shapefile.Reader(shpfile_polygons)
-    except shapefile.ShapefileException:
-        sf_points = shpfile_points
-        sf_polygons = shpfile_polygons
-
-    for shape in sf_points.shapes():
-        if projection is not None:
-            x, y = shape.points[0]
-            point = Point(pyproj.transform(projection, op.WGS84, x, y))
-        else:
-            point = Point(shape.points[0])
-        for poly in sf_polygons.shapes():
-            if all([len(x) > 2 for x in poly.points]):
-                poly = [[x, y] for x, y, _, _ in poly.points]
-                if Polygon(poly).contains(point):
+    if shpfile_shapes.shapeType == 1:
+        for shape in shpfile_shapes.shapes():
+            point = slgeo.Point(shape.points[0])
+            for poly in shpfile_polygons.shapes():
+                if all([len(x) > 2 for x in poly.points]):
+                    poly = [[x, y] for x, y, _, _ in poly.points]
+                    if slgeo.Polygon(poly).contains(point):
+                        coords.append(point.coords[0])
+                elif slgeo.Polygon(poly.points[0]).contains(point):
                     coords.append(point.coords[0])
-            elif Polygon(poly.points[0]).contains(point):
-                coords.append(point.coords[0])
-    return coords
+        return coords
+    else:
+        pass
 
+# def coords_in_polygon(shpfile_points, shpfile_polygons, projection=None):
+#
+#     coords = []
+#
+#     try:
+#         sf_points = shapefile.Reader(shpfile_points)
+#         sf_polygons = shapefile.Reader(shpfile_polygons)
+#     except shapefile.ShapefileException:
+#         sf_points = shpfile_points
+#         sf_polygons = shpfile_polygons
+#
+#     for shape in sf_points.shapes():
+#         if projection is not None:
+#             x, y = shape.points[0]
+#             point = Point(pyproj.transform(projection, op.WGS84, x, y))
+#         else:
+#             point = Point(shape.points[0])
+#         for poly in sf_polygons.shapes():
+#             if all([len(x) > 2 for x in poly.points]):
+#                 poly = [[x, y] for x, y, _, _ in poly.points]
+#                 if Polygon(poly).contains(point):
+#                     coords.append(point.coords[0])
+#             elif Polygon(poly.points[0]).contains(point):
+#                 coords.append(point.coords[0])
+#     return coords
