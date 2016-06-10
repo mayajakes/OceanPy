b891bca55fdd1886ab09234d8b36dc0d346c3198
janjaapmeijer <jaap.meijer@sei-international.org>
2016-06-10 11:45:05 +0200

    Implementing class Shapefile.

--- shapefiles.py
+++ shapefiles.py
@@ -70,7 +70,6 @@ def write_shpfile(coords, path=None):
         shpfile.point(x, y, z)
 
     if path is not None:
-        shpfile.field(list(('DeletionFlag', 'C', 1, 0)))
         shpfile.save(path)
 
     return shpfile
