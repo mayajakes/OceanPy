eed21da519cbc2122502395deef601a423b49cad
janjaapmeijer <jaap.meijer@sei-international.org>
2016-06-13 11:56:52 +0200

    .

--- shapefiles.py
+++ shapefiles.py
@@ -78,7 +78,7 @@ def shapes_in_polygons(shpfile_shapes, shpfile_polygons,):
                     shapes.point(x=shape.point[0][0], y=shape.point[0][1])
                     shapes.field(shpfile_shapes.fields[1][0], shpfile_shapes.fields[1][1],
                                  shpfile_shapes.fields[1][2], shpfile_shapes.fields[1][3])
-                    shapes.record(rec[0])
+                    shapes.record(rec[0][0])
 
                 for s in shapes.shapes():
                     s.shapeType = shpfile_shapes.shapeType
