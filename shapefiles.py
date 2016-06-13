92069a58f2ed0258e7fa5a4818df5f03fcb064a8
janjaapmeijer <jaap.meijer@sei-international.org>
2016-06-13 11:25:45 +0200

    .

--- shapefiles.py
+++ shapefiles.py
@@ -71,11 +71,11 @@ def shapes_in_polygons(shpfile_shapes, shpfile_polygons,):
                     poly = [[x, y] for x, y, _, _ in poly.points]
                     if slgeo.Polygon(poly).contains(point):
                         shapes.point(x=shape.points[0][0], y=shape.points[0][1])
-                        shapes.field(shpfile_shapes.fields[1:])
+                        shapes.field(shpfile_shapes)
                         shapes.record(rec)
                 elif slgeo.Polygon(poly.points[0]).contains(point):
                     shapes.point(x=shape.point[0][0], y=shape.point[0][1])
-                    shapes.field(shpfile_shapes.fields[1:])
+                    shapes.field(shpfile_shapes)
                     shapes.record(rec)
 
                 for s in shapes.shapes():
