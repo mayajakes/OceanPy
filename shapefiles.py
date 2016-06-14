bf1e8d347af45b7e240868c832222bdf7447677b
janjaapmeijer <jaap.meijer@sei-international.org>
2016-06-14 16:19:34 +0200

    .

--- shapefiles.py
+++ shapefiles.py
@@ -67,19 +67,19 @@ def shapes_in_polygons(shpfile_shapes, shpfile_polygons):
         if len(shpfile_shapes.fields) > 0:
             for field in shpfile_shapes.fields:
                 shapes.field(field[0], field[1], field[2], field[3])
-        # shapes.field(shpfile_shapes.fields[1][0], shpfile_shapes.fields[1][1],
-        #              shpfile_shapes.fields[1][2], shpfile_shapes.fields[1][3])
-        for shape, rec in zip(shpfile_shapes.shapes(), shpfile_shapes.records()):
+
+        for i, shape in enumerate(shpfile_shapes.shapes()):
             point = slgeo.Point(shape.points[0])
             for poly in shpfile_polygons.shapes():
                 if all([len(x) > 2 for x in poly.points]):
                     poly = [[x, y] for x, y, _, _ in poly.points]
                     if slgeo.Polygon(poly).contains(point):
                         shapes.point(x=shape.points[0][0], y=shape.points[0][1])
-                        shapes.record(rec[0], rec[1])
-                elif slgeo.Polygon(poly.points[0]).contains(point):
-                    shapes.point(x=shape.point[0][0], y=shape.point[0][1])
-                    shapes.record(rec[0], rec[1])
+                        if isinstance(shpfile_shapes.records, list) is False:
+                            shapes.record(shpfile_shapes.records()[i][0], shpfile_shapes.records()[i][0])
+                # elif slgeo.Polygon(poly.points[0]).contains(point):
+                #     shapes.point(x=shape.point[0][0], y=shape.point[0][1])
+                #     shapes.record(rec[0], rec[1])
 
                 for s in shapes.shapes():
                     s.shapeType = shpfile_shapes.shapeType
