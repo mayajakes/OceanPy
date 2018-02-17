b3cf5ae5b880f269424d2f9e929eb615a49f142f
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2018-02-17 18:15:52 +1100

    Add function for fitting a n-order polynomial to 1D-data.

--- polynomials.py
+++ polynomials.py
@@ -101,28 +101,69 @@ def polyfit_2d(x, y, z, order=1, gridsize=(50, 100)):
 
     return xm, ym, zm
 
+def polyfit1d(x, y, order=1, grid=True):
+    '''
+    Uses x, y data and fits polynomial using the least-squares method for given order.
+    Solves the polynomial of the form:
+    y = ax + b                              (order = 1 or 'linear')
+    y = ax^2 + bx + c                       (order = 2 or 'quadratic')
+    y = ax^3 + bx^2 + cx + d                (order = 3 or 'cubic'
+    '''
 
-def polyfit2d(x, y, z, order=2, gridsize=(50, 100)):
-    ''' built in residuals and RMSE '''
-
-    if gridsize is False:
-        xm, ym, zm = x.copy(), y.copy(), np.zeros(z.shape)
+    if grid:
+        xm, ym = np.linspace(x.min(), x.max(), len(y)), np.zeros(len(y))
     else:
+        xm, ym = x.copy(), np.zeros(len(y))
+
+    nterms = int(order + 1)
+
+    P = np.zeros((x.size, nterms))
+    pascal_triangle = [i for i in range(order + 1) if i <= order]
+    for k, i in enumerate(pascal_triangle):
+        P[:, k] = x ** i
+
+    A = np.linalg.lstsq(P, y)[0]
+    for alpha, i in zip(A, pascal_triangle):
+        ym += alpha * xm ** i
+
+    return xm, ym
+
+
+def polyfit2d(x, y, z, order=1, gridsize=False, point=False):
+    '''
+    Uses x, y and z data and fits polynomial using the least-squares method for given order.
+    Solves the polynomial of the form:
+    z = ax + by + c                                                     (order = 1 or 'linear')
+    y = ax^2 + by^2 + cxy + dx + ey + f                                 (order = 2 or 'quadratic')
+    y = ax^3 + by^3 + cxy^2 + dx^2y + ex^2 + fy^2 + gxy + hx + iy + j   (order = 3 or 'cubic')
+    :param x:
+    :param y:
+    :param z:
+    :param order:
+    :param gridsize:
+    :return:
+    '''
+
+    # TODO: built in residuals and RMSE
+
+    if point:
+        xm, ym, zm = point[0], point[1], 0
+    elif gridsize:
         xg, yg = np.meshgrid(np.linspace(x.min(), x.max(), gridsize[0]),
                              np.linspace(y.min(), y.max(), gridsize[1]))
         xm, ym = xg.flatten(), yg.flatten()
         zm = np.zeros(xm.shape)
+    else:
+        xm, ym, zm = x.copy(), y.copy(), np.zeros(z.shape)
 
     nterms = int((order ** 2 + 3 * order + 2) / 2)
 
     P = np.zeros((x.size, nterms))
-
     pascal_triangle = [(i, j) for i in range(order + 1) for j in range(order + 1) if i + j <= order]
-
     for k, (i, j) in enumerate(pascal_triangle):
         P[:, k] = x ** i * y ** j
-    A = np.linalg.lstsq(P, z)[0]
 
+    A = np.linalg.lstsq(P, z)[0]
     for alpha, (i, j) in zip(A, pascal_triangle):
         zm += alpha * xm ** i * ym ** j
 
