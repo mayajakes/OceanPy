b113dad1bcc234afd33908a011b83fbe30e0f766
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2019-05-06 15:33:39 -0400

    Adjusted OI function, with multivariate analysis for geostrophy.

--- interpolation.py
+++ interpolation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
+import numpy.matlib as npmatlib
 
 def interpolate_1h(df, n):
 
@@ -113,7 +114,7 @@ def polyfit2d(x, y, z, order=1):
 
     return get_zz#, rmse
 
-def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, gridsize=(20, 20)):
+def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, mvoi=False, f_cor=False, order=1, gridsize=(20, 20)):
     '''
     Optimal Interpolation scheme based on Kalnay, 2003
     Multivariate analysis: http://www.atmosp.physics.utoronto.ca/PHY2509/ch3.pdf
@@ -124,16 +125,27 @@ def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, grids
     :param L:
     :param xx:
     :param yy:
-    :param bg_fld:
+    :param bg_fld: tuple of background fields
+    :param mvoi: 'geostrophy' or 'potential_vorticity'
     :param gridsize:
     :return:
     '''
+
+    # check if observation field has the same shape
+    if x.shape != obs_fld.shape:
+        raise ValueError('x, y and obs_fld must have same dimensions')
 
     # checks if background field is provided
     lst = [xx, yy, bg_fld]
     if all(v is not None for v in lst):
 
-        ny, nx = bg_fld.shape
+        # use dimensions of field, in case bg_fld is tuple
+        try:
+            ny, nx = bg_fld.shape
+        except AttributeError:
+            ny, nx = bg_fld[0].shape
+
+        # make grid if grid dimensions are 1-dimensional
         if xx.ndim == 1 and yy.ndim == 1:
             xi, yi = xx.copy(), yy.copy()
             xx, yy = np.meshgrid(xi, yi)
@@ -144,7 +156,7 @@ def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, grids
         else:
             raise InputError('Optimal interpolation works only for 1 or 2-dimensional arrays')
 
-        # Lx, Ly = abs(xi[-1] - xi[0]), abs(yi[-1] - yi[0])
+        # lx, ly = abs(xi[-1] - xi[0]), abs(yi[-1] - yi[0])
         dx, dy = abs(xi[-1] - xi[0]) / (nx - 1), abs(yi[-1] - yi[0]) / (ny - 1)
         xc, yc = xi[0] + (nx - 1) * dx / 2, yi[0] + (ny - 1) * dy / 2
 
@@ -154,7 +166,8 @@ def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, grids
         ny, nx = gridsize
         xi, dx = np.linspace(min(x), max(x), nx, retstep=True)
         yi, dy = np.linspace(min(y), max(y), ny, retstep=True)
-        # Lx, Ly = abs(max(x) - min(x)), abs(max(y) - min(y))
+        # lx, ly = abs(max(x) - min(x)), abs(max(y) - min(y))
+        # dx, dy = lx / (nx - 1), ly / (ny - 1)
         xx, yy = np.meshgrid(xi, yi)
         xc, yc = xi[0] + (nx - 1) * dx / 2, yi[0] + (ny - 1) * dy / 2
 
@@ -164,7 +177,9 @@ def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, grids
     else:
         raise InputError('If background field is provided, grid (xx, yy) should be provided too.')
 
-    N = nx * ny
+    nvar = 3 if mvoi=='geostrophy' else 1
+    n = nx * ny
+    N = n * nvar
     P = len(obs_fld)
 
     # BACKGROUND ERROR COVARIANCE MATRIX
@@ -175,44 +190,69 @@ def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, grids
 
     def Bmatrix(varian_b, Lx, Ly=Ly):
 
-        B = np.matlib.ones((N, N))
-        for m in range(1, N):
-            mj = int(m / nx)
-            mi = m - mj * nx
+        B = npmatlib.zeros((n, n))
+        for l in range(1, n):
+            lj = int(l / nx)
+            li = l - lj * nx
 
-            xm = xc + (mi - int(nx / 2)) * dx
-            ym = yc + (mj - int(ny / 2)) * dy
+            xl = xc + (li - int(nx / 2)) * dx
+            yl = yc + (lj - int(ny / 2)) * dy
 
-            for l in range(0, m):
-                lj = int(l / nx)
-                li = l - lj * nx
+            for m in range(0, l):
+                mj = int(m / nx)
+                mi = m - mj * nx
 
-                xl = xc + (li - int(nx / 2)) * dx
-                yl = yc + (lj - int(ny / 2)) * dy
+                xm = xc + (mi - int(nx / 2)) * dx
+                ym = yc + (mj - int(ny / 2)) * dy
 
-                dist2 = (xm - xl) ** 2 + (ym - yl) ** 2
+                dist2 = (xl - xm) ** 2 + (yl - ym) ** 2
                 cov = np.exp(-dist2 / (2 * Lx ** 2)) if Ly is None else np.exp(-dist2 / (Lx**2 + Ly**2))
-                B[m, l] = cov
                 B[l, m] = cov
+                B[m, l] = cov
 
         # variance background field
-        for m in range(0, N):
-            B[m, m] = varian_b
+        for l in range(0, n):
+            B[l, l] = varian_b
 
         return B
 
-    varian_b = np.var(bg_fld)
+    varian_b = 1#np.var(bg_fld)
     B = Bmatrix(varian_b, Lx, Ly)
+
+    if mvoi=='geostrophy':
+
+        BB = np.reshape(np.asarray(B), (ny, nx, ny, nx))
+        pu = np.empty((ny, nx, ny, nx))
+        pv, uu, vv, uv = pu.copy(), pu.copy(), pu.copy(), pu.copy()
+        for i in range(nx):
+            for j in range(ny):
+                # pp = BB[j, i]
+                pu[j, i] = (1 / f_cor) * np.gradient(BB[j, i], dy)[0]
+                pv[j, i] = - (1 / f_cor) * np.gradient(BB[j, i], dx)[1]
+                uu[j, i] = - (1 / f_cor ** 2) * np.gradient(np.gradient(BB[j, i], dy)[0], dy)[0]
+                vv[j, i] = - (1 / f_cor ** 2) * np.gradient(np.gradient(BB[j, i], dx)[1], dx)[1]
+                uv[j, i] = (1 / f_cor ** 2) * np.gradient(np.gradient(BB[j, i], dx)[0], dy)[1]
+
+        B = np.empty((N, N))
+        B[0::nvar, 0::nvar] = BB.reshape((n, n))
+        B[1::nvar, 0::nvar] = pu.reshape((n, n))
+        B[0::nvar, 1::nvar] = -pu.reshape((n, n))
+        B[2::nvar, 0::nvar] = pv.reshape((n, n))
+        B[0::nvar, 2::nvar] = -pv.reshape((n, n))
+        B[1::nvar, 1::nvar] = uu.reshape((n, n))
+        B[2::nvar, 2::nvar] = vv.reshape((n, n))
+        B[2::nvar, 1::nvar] = uv.reshape((n, n))
+        B[1::nvar, 2::nvar] = uv.reshape((n, n))
 
     # OBSERVATION ERROR COVARIANCE MATRIX
     varian_r = np.var(obs_fld)
     R = np.identity(P)
-    R = varian_r * R
+    # R = varian_r * R
 
     # FORWARD OPERATOR OR OBSERVATION OPERATOR MATRIX
     def Hmatrix():
 
-        H = np.matlib.zeros((P, N))
+        H = npmatlib.zeros((P, N))
         for k in range(P):
 
             # llcrnr of grid cell
@@ -224,21 +264,21 @@ def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, grids
 
             if 0 <= i <= nx - 1 and 0 <= j <= ny - 1:
 
+                # use the right index at the top and right boundary
+                i = i - 1 if i == nx - 1 else i
+                j = j - 1 if j == ny - 1 else j
+
                 # normalized weighting factor in x, y direction
                 wx = xo - i
                 wy = yo - j
 
-                i = i - 1 if i == nx - 1 else i
-                j = j - 1 if j == ny - 1 else j
-
                 # fill matrix with weighting factors
-                H[k, j * nx + i] = (1 - wx) * (1 - wy)
-                H[k, j * nx + i + 1] = wx * (1 - wy)
-                H[k, j * nx + nx + i] = (1 - wx) * wy
-                H[k, j * nx + nx + i + 1] = wx * wy
+                H[k, nvar * (j * nx + i)] = (1 - wx) * (1 - wy)
+                H[k, nvar * (j * nx + i + 1)] = wx * (1 - wy)
+                H[k, nvar * (j * nx + nx + i)] = (1 - wx) * wy
+                H[k, nvar * (j * nx + nx + i + 1)] = wx * wy
 
                 # print('Check sum: %s' % ((1 - wx) * (1 - wy) + wx * (1 - wy) + (1 - wx) * wy + wx * wy))
-
 
             else:
                 raise ValueError('Observation point (%s, %s) is not within grid domain.' % (x[k], y[k]))
@@ -248,13 +288,19 @@ def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, grids
 
 
     # BACKGROUND FIELD VECTOR AT GRID POINTS
-    x_b = np.reshape(bg_fld, (N, 1))
+    # x_b = np.reshape(bg_fld, (N, 1))
+    x_b = npmatlib.empty((N, 1))
+    for ivar in range(nvar):
+        if type(bg_fld) == tuple:
+            x_b[ivar::nvar] = np.reshape(bg_fld[ivar], (n, 1))
+        else:
+            x_b[ivar::nvar] = np.reshape(bg_fld, (n, 1))
+
 
     # BACKGROUND FIELD VECTOR AT OBSERVATION POINTS
     y_b = H * x_b
 
     # if convert:
-        
 
     # OBSERVATION FIELD VECTOR AT OBSERVATION POINTS
     y_o = np.matrix(obs_fld).T
@@ -269,11 +315,21 @@ def OI(x, y, obs_fld, Lx, Ly=None, xx=None, yy=None, bg_fld=None, order=1, grids
     x_a = x_b + W * d
 
     # reshape analysis filed vector into grid array and make matrix an array
-    ana_fld = np.asarray(np.reshape(x_a, (ny, nx)))
+    # ana_fld = np.asarray(np.reshape(x_a, (ny, nx)))
+    ana_fld = ()
+    for ivar in range(nvar):
+        ana_fld += (np.asarray(np.reshape(x_a[ivar::nvar], (ny, nx))),)
+
+    # check if bg_fld and ana_fld are tuples of length 1
+    if len(ana_fld) == 1 and type(ana_fld) == tuple:
+        ana_fld = ana_fld[0]
+    if len(bg_fld) == 1 and type(bg_fld) == tuple:
+        bg_fld = bg_fld[0]
+
+    bg_obs = np.asarray(y_b).T
 
     # ANALYSIS ERROR COVARIANCE MATRIX
     I = np.identity(N)
     A = (I - W * H) * B
 
-    return xx, yy, bg_fld, ana_fld
-
+    return xx, yy, bg_fld, ana_fld, bg_obs, B, A
